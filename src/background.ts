d2b3ba80984e32226dde2b5f39ee9799f2362983
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-07-28 11:38:33 +0800

    Enable summary plugin

--- src/background.ts
+++ src/background.ts
@@ -5,3 +5,6 @@ console.log("Hello from the background!");
 browser.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
 });
+
+// 集成文本总结功能
+import "./plugins/text-summary/background-extension";
